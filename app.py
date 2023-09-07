27afbf7ce568e2765fae4b99bce60b981cb9aac6
carozum <czumbiehlfaure@hotmail.fr>
2023-09-07 09:09:35 +0200

    add the try except else error handling in the main menu

--- app.py
+++ app.py
@@ -21,28 +21,53 @@ def main():
     
     while True:
         main_menu()
-        answer = input("Enter an option A or Q:    ").upper()
+        answer = input("Enter an option A or Q:    ")
         
-        if answer == 'A':
-            team_menu(teams)
-            team_selected = input("Choose a team:   ").upper()
-            #display stats
-            if team_selected == "A":
-                team_A = players_by_team[0]
-                print_stats(team_A)
-            elif team_selected == "B":
-                team_B = players_by_team[1]
-                print_stats(team_B)
-            elif team_selected == "C":
-                team_C = players_by_team[2]
-                print_stats(team_C)
-            else: 
-                continue
-            
+        try:
+            if answer.upper() != 'A' and answer.upper() != 'Q':
+                raise ValueError("\n******** You have to choose between A or Q *********\n")
+        except ValueError as err: 
+            print(err)
         else: 
-            break
+            if answer.upper() == 'Q':
+                print("\n********* Thank you for using the BASKETBALL TEAM STATS TOOL. See you later ****************\n")
+                break
+            if answer.upper() == 'A':
+                team_menu(teams)
+                team_selected = input("Choose a team:   ").upper()
+                #display stats
+                if team_selected == "A":
+                    team_A = players_by_team[0]
+                    print_stats(team_A)
+                elif team_selected == "B":
+                    team_B = players_by_team[1]
+                    print_stats(team_B)
+                elif team_selected == "C":
+                    team_C = players_by_team[2]
+                    print_stats(team_C)
+                else: 
+                    print("\n****** You have not chosen a correct letter for the team. ****\n")
+                    continue
+            
+        
+        # if answer.upper() == 'A':
+        #     team_menu(teams)
+        #     team_selected = input("Choose a team:   ").upper()
+        #     #display stats
+        #     if team_selected == "A":
+        #         team_A = players_by_team[0]
+        #         print_stats(team_A)
+        #     elif team_selected == "B":
+        #         team_B = players_by_team[1]
+        #         print_stats(team_B)
+        #     elif team_selected == "C":
+        #         team_C = players_by_team[2]
+        #         print_stats(team_C)
+        #     else: 
+        #         continue
+            
+        # else: 
+        #     break
 
 if __name__ == '__main__':
     main()
-
-    
